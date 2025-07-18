6a0df85a4428f6826e7b3307c5f5aa3f6759a207
personalstrategy <personalstrategy@protonmail.com>
2025-07-18 11:19:40 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+            apiKey: "AIzaSyCUdvZzxrR3vxwoA5XE_j3dIm6SihQu-s0",
+            authDomain: "groceries-list-2c49d.firebaseapp.com",
+            projectId: "groceries-list-2c49d",
+            storageBucket: "groceries-list-2c49d.firebasestorage.app",
+            messagingSenderId: "679441493156",
+            appId: "1:679441493156:web:4917bf068234b20e67ca31"
+        };
